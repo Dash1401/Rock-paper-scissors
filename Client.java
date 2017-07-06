b7753df07524f2d14bb7a59c494fa24b74e40da2
svens <svens@DESKTOP-MJ142AU>
2017-07-06 21:18:19 +0200

    Upload new Version

--- Client.java
+++ Client.java
@@ -1,1 +1,13 @@
-
+public class Client
+{
+	private static String Auswahl;
+	public static void main(String[] args)
+	{
+		Auswahl = args[0];
+		
+		OutputStream out = recv.getOutputStream();
+		PrintWriter writer = new PrintWriter(out);
+		
+		writer.write(Auswahl);
+	}
+}
--- Server.java
+++ Server.java
@@ -1,1 +1,39 @@
+public static void main(String[] args)
+{
+	public static void main(String[] args) {
+		try {
+			ServerSocket server = new ServerSocket(3000);
+			System.out.println("Server gestartet");
+			Socket recv = server.accept();
+			System.out.println("Test");
 
+			//Streams
+			
+			OutputStream out = recv.getOutputStream();
+			PrintWriter writer = new PrintWriter(out);
+			
+			InputStream in = recv.getInputStream();
+			BufferedReader buf = new BufferedReader(new InputStreamReader(in));
+			
+			String s = null;
+			int count = 0;
+			while((s = buf.readLine()) != null)
+			{
+				writer.write("Daten die empfangen wurden: " + s + "\n" + "Anzahl der Daten: " + count);
+				writer.flush();
+				System.out.println(s);
+				
+			}
+			
+			
+			
+			writer.close();
+			buf.close();
+			server.close();
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+}
+}
